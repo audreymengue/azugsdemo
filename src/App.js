349ee4c87ad5ebfdc522503562fb53f266d325a3
Audrey Mengue <massoumeharmonie@gmail.com>
2022-05-25 16:51:35 +0000

    azugs demo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <main>
+      <section className='container'>
+        <h3>Hey, CI/CD here with Github Actions</h3>
+       
+      </section>
+    </main>
+  )
+}
+
+export default App
